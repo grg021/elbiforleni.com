fab41a6be40272ec9b71fca29c9dcaeb452da509
Greg Hermo <greg.hermo@gmail.com>
2021-12-03 20:06:32 +0800

    use pink lugaw icon

--- components/icons/ico-lugaw.js
+++ components/icons/ico-lugaw.js
@@ -1,8 +1,8 @@
 import { icon } from "leaflet"
 
 const IcoLugaw = icon({
-  iconUrl: "/assets/icons/lugaw-icon.png",
-  iconSize: [32, 32],
+  iconUrl: "/assets/icons/marker-lugaw.png",
+  iconSize: [24, 29],
 })
 
 export default IcoLugaw;
