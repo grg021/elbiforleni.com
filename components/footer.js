697b972ad77a38fc10674e462b580a2187d72fba
Greg Hermo <greg.hermo@gmail.com>
2021-12-03 20:56:15 +0800

    update footer content

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,32 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import {InternalLink, HashTag} from '../components/styled'
+import Image from 'next/image'
 
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-3xl lg:text-4xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Powered by kakampinks.
-          </h3>
+        <div className="py-16 w-full max-w-3xl mx-auto grid grid-cols-1 gap-4 sm:grid-cols-3">
+          <div className="flex flex-col space-y-4">
+            <div className="mr-8 relative w-40 h-40">
+              <Image width={300} height={300} layout="fill" src="/assets/homepage/lb-for-leni-logo.jpeg" alt="lb leni logo" />
+            </div>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <div className="font-medium tracking-wide text-pink-500">About</div>
+            <div className="mr-8 leading-loose text-gray-600">
+              Nagkakaisang mga taga-suporta ni Vice PRESIDENT Leni Robredo sa Los Banos, Laguna. {' '}
+              <HashTag>#LabanLeni2022</HashTag>{' '}
+              <HashTag>#MatinoAtMahusay</HashTag>{' '}
+              <HashTag>#LBForLeni</HashTag>
+            </div>
+          </div>
+          
+          <div className="flex flex-col space-y-4">
+          <div className="font-medium tracking-wide text-pink-500">Links</div>
+            <InternalLink target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/LBForLeni/">Facebook</InternalLink>
+          </div>
+          
         </div>
       </Container>
     </footer>
---
+++ components/styled.tsx
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components"
+
+export const InternalLink = tw.a`
+  text-gray-500 hover:text-gray-600 transition underline
+`
+
+export const HashTag = tw.span`
+  text-white bg-pink-400 p-1 rounded-sm hover:text-pink-100 transition
+`
