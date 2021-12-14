548bb6c0ce3ea11c5cd6177c5a80be492f828bda
Darla Grace Hermo <darla@leafnode.io>
2021-12-14 19:49:09 +0800

    Update map legend and markers. Add sorbetes marker

---
+++ components/icons/ico-icecream.js
@@ -0,0 +1,8 @@
+import { icon } from "leaflet"
+
+const IcoIceCream = icon({
+  iconUrl: "/assets/icons/icecream-marker.png",
+  iconSize: [42, 35],
+})
+
+export default IcoIceCream;
--- components/map-legend.js
+++ components/map-legend.js
@@ -9,14 +9,18 @@ const MapLegend = () => {
     return <div className={'flex flex-col my-8 gap-x-5 max-w-4xl mx-auto sm:flex-row gap-y-5'}>
       <LegendContainer>
         <span className="w-10 text-center"><Image src="/assets/icons/marker-lugaw.png" alt="lugaw icon" width="32" height="35" /></span>
-        <span className="ml-2">Kusina ni Leni / Lugaw ni Leni</span>
+        <span className="ml-2">Kusina / Lugaw ni Leni</span>
       </LegendContainer>
       <LegendContainer>
-        <span className="w-10 text-center"><Image src="/assets/icons/cafe-icon.png" alt="lugaw icon" width="35" height="35" /></span>
-        <span className="ml-2">Libreng Taho / Libreng Kape ni Leni-Kiko</span>
+        <span className="w-10 text-center"><Image src="/assets/icons/cafe-icon.png" alt="taho icon" width="35" height="35" /></span>
+        <span className="ml-2">Libreng Taho / Kapehan</span>
       </LegendContainer>
       <LegendContainer>
-        <span className="w-10 text-center"><Image src="/assets/icons/car-icon.png" alt="lugaw icon" width="23" height="35" /></span>
+        <span className="w-10 text-center"><Image src="/assets/icons/icecream-marker.png" alt="sorbetes icon" width="42" height="35" /></span>
+        <span className="ml-2">Libreng Sorbetes</span>
+      </LegendContainer>
+      <LegendContainer>
+        <span className="w-10 text-center"><Image src="/assets/icons/car-icon.png" alt="caravanicon" width="23" height="35" /></span>
         <span className="ml-2">Leni-Kiko Caravan</span>
       </LegendContainer>
     </div>
